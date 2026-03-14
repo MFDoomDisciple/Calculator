2e021d107200eca41482d3402d5f7c991e1171e3
MFDoomDisciple <carlosldlcruz@icloud.com>
2026-03-14 21:01:21 +0000

    Basic parse working

--- main.py
+++ main.py
@@ -1,3 +1,60 @@
+def parse(tokens):
+    return parse_add_sub(tokens)[0]
+
+def parse_parens(tokens):
+    pass
+
+def parse_expr(tokens):
+    pass
+
+def parse_mult_div(tokens):
+    (a, tokens) = parse_term(tokens)
+    if not a:
+        return (None, tokens)
+    (op, tokens) = parse_symbol(tokens, "*/")
+    if not op:
+        return (a, tokens)
+    (b, tokens) = parse_mult_div(tokens)
+    if not b:
+        raise ValueError(f"{op} requires a right side")
+    match op:
+        case "*": return (a*b, tokens)
+        case "/": return (a/b, tokens)
+
+def parse_add_sub(tokens):
+    (a, tokens) = parse_mult_div(tokens)
+    if not a:
+        return (None, tokens)
+    (op, tokens) = parse_symbol(tokens, "+-")
+    if not op:
+        return (a, tokens)
+    (b, tokens) = parse_add_sub(tokens)
+    if not b:
+        raise ValueError(f"{op} requires a right side")
+    match op:
+        case "+": return (a+b, tokens)
+        case "-": return (a-b, tokens)
+
+def parse_term(tokens):
+    return parse_num(tokens)
+
+def parse_num(tokens):
+    if len(tokens) == 0:
+        return (None, tokens)
+    if isinstance(tokens[0], int):
+        return (tokens[0], tokens[1:])
+    else:
+        return (None, tokens)
+
+def parse_symbol(tokens, symbols):
+    if len(tokens) == 0:
+        return (None, tokens)
+
+    if tokens[0] in symbols:
+        return (tokens[0], tokens[1:])
+    else:
+        return (None, tokens)
+
 def lex(inp):
     tokens = []
     i = 0
@@ -16,7 +73,8 @@ def lex(inp):
         else:
             i += 1
     return tokens
+
         
 while True:
     inp = input("> ")
-    print(lex(inp))
+    print(parse(lex(inp)))
